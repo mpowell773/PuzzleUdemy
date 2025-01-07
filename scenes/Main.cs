19a0c20b749bfa43f27d053115b6627c43fe89a9
Matt Powell <96788296+mpowell773@users.noreply.github.com>
2025-01-07 11:27:02 -0500

    Add basic grid snapping to cursor

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
 
+namespace Game;
+
 public partial class Main : Node2D
 {
+	private Sprite2D sprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello world");
+		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
+	{	
+		// Get grid coordinates.
+		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 gridPosition = mousePosition / 64;
+		gridPosition = gridPosition.Floor();
+		// Update sprite cursor to snap to grid.
+		sprite.GlobalPosition = gridPosition * 64;
 	}
 }
