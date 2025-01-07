95d41f5e373f84c44e6ada55df6dbd06b5d4fc4f
Matt Powell <96788296+mpowell773@users.noreply.github.com>
2025-01-07 11:40:49 -0500

    Add building instantiation to the grid system

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -6,21 +6,46 @@ namespace Game;
 public partial class Main : Node2D
 {
 	private Sprite2D sprite;
+	private PackedScene buildingScene;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _UnhandledInput(InputEvent evt)
+    {
+        if (evt.IsActionPressed("left_click"))
+		{
+			PlaceBuildingAtMousePosition();
+		}
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{	
-		// Get grid coordinates.
+		Vector2 gridPosition = GetMouseGridCellPosition();
+		// Update sprite cursor to snap to grid.
+		sprite.GlobalPosition = gridPosition * 64;
+	}
+
+	private Vector2 GetMouseGridCellPosition()
+	{
+				// Get grid coordinates.
 		Vector2 mousePosition = GetGlobalMousePosition();
 		Vector2 gridPosition = mousePosition / 64;
 		gridPosition = gridPosition.Floor();
-		// Update sprite cursor to snap to grid.
-		sprite.GlobalPosition = gridPosition * 64;
+		return gridPosition;
+	}
+
+	private void PlaceBuildingAtMousePosition()
+	{
+		Node2D building = buildingScene.Instantiate<Node2D>();
+		AddChild(building);
+
+		Vector2 gridPosition = GetMouseGridCellPosition();
+		building.GlobalPosition = gridPosition * 64;
 	}
 }
