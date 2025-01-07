53b71373f72278f28d8a2c1bc2977e3a4803e433
Matt Powell <96788296+mpowell773@users.noreply.github.com>
2025-01-07 14:21:16 -0500

    Add hashset to account if cell is occupied

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace Game;
 
@@ -11,6 +12,7 @@ public partial class Main : Node2D
 	private TileMapLayer highlightTilemapLayer;
 
 	private Vector2? hoveredGridCell;
+	private HashSet<Vector2> occupiedCells = new();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -30,7 +32,7 @@ public partial class Main : Node2D
 
 	public override void _UnhandledInput(InputEvent evt)
 	{
-		if (cursor.Visible && evt.IsActionPressed("left_click"))
+		if (cursor.Visible && evt.IsActionPressed("left_click") && !occupiedCells.Contains(GetMouseGridCellPosition()))
 		{
 			PlaceBuildingAtMousePosition();
 			cursor.Visible = false;
@@ -67,6 +69,7 @@ public partial class Main : Node2D
 
 		Vector2 gridPosition = GetMouseGridCellPosition();
 		building.GlobalPosition = gridPosition * 64;
+		occupiedCells.Add(gridPosition);
 
 		// Building is placed so hovered cell can be nullified.
 		hoveredGridCell = null;
